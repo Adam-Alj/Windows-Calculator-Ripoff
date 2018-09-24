1dbeca084fa9d729ce782a9ef0793377af8b1812
Unknown <adamaljumaily@gmail.com>
2018-09-24 20:02:43 -0300

    Added main files

---
+++ Form1.designer.vb
@@ -0,0 +1,340 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.bDiv = New System.Windows.Forms.Button()
+        Me.bMult = New System.Windows.Forms.Button()
+        Me.bMinus = New System.Windows.Forms.Button()
+        Me.eqBtn = New System.Windows.Forms.Button()
+        Me.b9 = New System.Windows.Forms.Button()
+        Me.b6 = New System.Windows.Forms.Button()
+        Me.b0 = New System.Windows.Forms.Button()
+        Me.b8 = New System.Windows.Forms.Button()
+        Me.b5 = New System.Windows.Forms.Button()
+        Me.bDot = New System.Windows.Forms.Button()
+        Me.b7 = New System.Windows.Forms.Button()
+        Me.b4 = New System.Windows.Forms.Button()
+        Me.bPlus = New System.Windows.Forms.Button()
+        Me.b3 = New System.Windows.Forms.Button()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.prevCalc = New System.Windows.Forms.Label()
+        Me.clrBtn = New System.Windows.Forms.Button()
+        Me.bkBtn = New System.Windows.Forms.Button()
+        Me.signBtn = New System.Windows.Forms.Button()
+        Me.exitBtn = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.bDiv)
+        Me.Panel1.Controls.Add(Me.bMult)
+        Me.Panel1.Controls.Add(Me.bMinus)
+        Me.Panel1.Controls.Add(Me.eqBtn)
+        Me.Panel1.Controls.Add(Me.b9)
+        Me.Panel1.Controls.Add(Me.b6)
+        Me.Panel1.Controls.Add(Me.b0)
+        Me.Panel1.Controls.Add(Me.b8)
+        Me.Panel1.Controls.Add(Me.b5)
+        Me.Panel1.Controls.Add(Me.bDot)
+        Me.Panel1.Controls.Add(Me.b7)
+        Me.Panel1.Controls.Add(Me.b4)
+        Me.Panel1.Controls.Add(Me.bPlus)
+        Me.Panel1.Controls.Add(Me.b3)
+        Me.Panel1.Controls.Add(Me.b2)
+        Me.Panel1.Controls.Add(Me.b1)
+        Me.Panel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(0, 177)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(445, 339)
+        Me.Panel1.TabIndex = 0
+        '
+        'bDiv
+        '
+        Me.bDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bDiv.Location = New System.Drawing.Point(335, 248)
+        Me.bDiv.Name = "bDiv"
+        Me.bDiv.Size = New System.Drawing.Size(85, 60)
+        Me.bDiv.TabIndex = 15
+        Me.bDiv.Text = "/"
+        Me.bDiv.UseVisualStyleBackColor = True
+        '
+        'bMult
+        '
+        Me.bMult.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bMult.Location = New System.Drawing.Point(335, 172)
+        Me.bMult.Name = "bMult"
+        Me.bMult.Size = New System.Drawing.Size(85, 60)
+        Me.bMult.TabIndex = 14
+        Me.bMult.Text = "*"
+        Me.bMult.UseVisualStyleBackColor = True
+        '
+        'bMinus
+        '
+        Me.bMinus.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bMinus.Location = New System.Drawing.Point(335, 94)
+        Me.bMinus.Name = "bMinus"
+        Me.bMinus.Size = New System.Drawing.Size(85, 60)
+        Me.bMinus.TabIndex = 13
+        Me.bMinus.Text = "-"
+        Me.bMinus.UseVisualStyleBackColor = True
+        '
+        'eqBtn
+        '
+        Me.eqBtn.Location = New System.Drawing.Point(220, 248)
+        Me.eqBtn.Name = "eqBtn"
+        Me.eqBtn.Size = New System.Drawing.Size(85, 60)
+        Me.eqBtn.TabIndex = 12
+        Me.eqBtn.Text = "="
+        Me.eqBtn.UseVisualStyleBackColor = True
+        '
+        'b9
+        '
+        Me.b9.Location = New System.Drawing.Point(220, 172)
+        Me.b9.Name = "b9"
+        Me.b9.Size = New System.Drawing.Size(85, 60)
+        Me.b9.TabIndex = 11
+        Me.b9.Text = "9"
+        Me.b9.UseVisualStyleBackColor = True
+        '
+        'b6
+        '
+        Me.b6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b6.Location = New System.Drawing.Point(220, 94)
+        Me.b6.Name = "b6"
+        Me.b6.Size = New System.Drawing.Size(85, 60)
+        Me.b6.TabIndex = 10
+        Me.b6.Text = "6"
+        Me.b6.UseVisualStyleBackColor = True
+        '
+        'b0
+        '
+        Me.b0.Location = New System.Drawing.Point(115, 248)
+        Me.b0.Name = "b0"
+        Me.b0.Size = New System.Drawing.Size(85, 60)
+        Me.b0.TabIndex = 9
+        Me.b0.Text = "0"
+        Me.b0.UseVisualStyleBackColor = True
+        '
+        'b8
+        '
+        Me.b8.Location = New System.Drawing.Point(115, 172)
+        Me.b8.Name = "b8"
+        Me.b8.Size = New System.Drawing.Size(85, 60)
+        Me.b8.TabIndex = 8
+        Me.b8.Text = "8"
+        Me.b8.UseVisualStyleBackColor = True
+        '
+        'b5
+        '
+        Me.b5.Location = New System.Drawing.Point(115, 94)
+        Me.b5.Name = "b5"
+        Me.b5.Size = New System.Drawing.Size(85, 60)
+        Me.b5.TabIndex = 7
+        Me.b5.Text = "5"
+        Me.b5.UseVisualStyleBackColor = True
+        '
+        'bDot
+        '
+        Me.bDot.Location = New System.Drawing.Point(14, 248)
+        Me.bDot.Name = "bDot"
+        Me.bDot.Size = New System.Drawing.Size(85, 60)
+        Me.bDot.TabIndex = 6
+        Me.bDot.Text = "."
+        Me.bDot.UseVisualStyleBackColor = True
+        '
+        'b7
+        '
+        Me.b7.Location = New System.Drawing.Point(14, 172)
+        Me.b7.Name = "b7"
+        Me.b7.Size = New System.Drawing.Size(85, 60)
+        Me.b7.TabIndex = 5
+        Me.b7.Text = "7"
+        Me.b7.UseVisualStyleBackColor = True
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(14, 94)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(85, 60)
+        Me.b4.TabIndex = 4
+        Me.b4.Text = "4"
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'bPlus
+        '
+        Me.bPlus.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bPlus.Location = New System.Drawing.Point(335, 17)
+        Me.bPlus.Name = "bPlus"
+        Me.bPlus.Size = New System.Drawing.Size(85, 60)
+        Me.bPlus.TabIndex = 3
+        Me.bPlus.Text = "+"
+        Me.bPlus.UseVisualStyleBackColor = True
+        '
+        'b3
+        '
+        Me.b3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b3.Location = New System.Drawing.Point(220, 17)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(85, 60)
+        Me.b3.TabIndex = 2
+        Me.b3.Text = "3"
+        Me.b3.UseVisualStyleBackColor = True
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(115, 17)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(85, 60)
+        Me.b2.TabIndex = 1
+        Me.b2.Text = "2"
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b1
+        '
+        Me.b1.CausesValidation = False
+        Me.b1.Location = New System.Drawing.Point(14, 17)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(85, 60)
+        Me.b1.TabIndex = 0
+        Me.b1.Text = "1"
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(9, 61)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(420, 80)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'prevCalc
+        '
+        Me.prevCalc.AutoSize = True
+        Me.prevCalc.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.prevCalc.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.prevCalc.Location = New System.Drawing.Point(181, 29)
+        Me.prevCalc.Name = "prevCalc"
+        Me.prevCalc.Size = New System.Drawing.Size(19, 29)
+        Me.prevCalc.TabIndex = 2
+        Me.prevCalc.Text = " "
+        '
+        'clrBtn
+        '
+        Me.clrBtn.Location = New System.Drawing.Point(14, 147)
+        Me.clrBtn.Name = "clrBtn"
+        Me.clrBtn.Size = New System.Drawing.Size(85, 24)
+        Me.clrBtn.TabIndex = 3
+        Me.clrBtn.Text = "CE"
+        Me.clrBtn.UseVisualStyleBackColor = True
+        '
+        'bkBtn
+        '
+        Me.bkBtn.Location = New System.Drawing.Point(115, 147)
+        Me.bkBtn.Name = "bkBtn"
+        Me.bkBtn.Size = New System.Drawing.Size(85, 24)
+        Me.bkBtn.TabIndex = 4
+        Me.bkBtn.Text = "<<"
+        Me.bkBtn.UseVisualStyleBackColor = True
+        '
+        'signBtn
+        '
+        Me.signBtn.Location = New System.Drawing.Point(220, 147)
+        Me.signBtn.Name = "signBtn"
+        Me.signBtn.Size = New System.Drawing.Size(85, 24)
+        Me.signBtn.TabIndex = 5
+        Me.signBtn.Text = "+ -"
+        Me.signBtn.UseVisualStyleBackColor = True
+        '
+        'exitBtn
+        '
+        Me.exitBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitBtn.Location = New System.Drawing.Point(409, 0)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(29, 28)
+        Me.exitBtn.TabIndex = 6
+        Me.exitBtn.Text = "X"
+        Me.exitBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.exitBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(438, 516)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.exitBtn)
+        Me.Controls.Add(Me.signBtn)
+        Me.Controls.Add(Me.bkBtn)
+        Me.Controls.Add(Me.clrBtn)
+        Me.Controls.Add(Me.prevCalc)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = " "
+        Me.TransparencyKey = System.Drawing.SystemColors.InactiveBorder
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents bDiv As Button
+    Friend WithEvents bMult As Button
+    Friend WithEvents bMinus As Button
+    Friend WithEvents eqBtn As Button
+    Friend WithEvents b9 As Button
+    Friend WithEvents b6 As Button
+    Friend WithEvents b0 As Button
+    Friend WithEvents b8 As Button
+    Friend WithEvents b5 As Button
+    Friend WithEvents bDot As Button
+    Friend WithEvents b7 As Button
+    Friend WithEvents b4 As Button
+    Friend WithEvents bPlus As Button
+    Friend WithEvents b3 As Button
+    Friend WithEvents b2 As Button
+    Friend WithEvents b1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents prevCalc As Label
+    Friend WithEvents clrBtn As Button
+    Friend WithEvents bkBtn As Button
+    Friend WithEvents signBtn As Button
+    Friend WithEvents exitBtn As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,330 @@
+﻿Public Class Form1
+
+    Dim op1 As Double
+    Dim operate As Char
+    Dim op2 As Double
+    Dim evaluated As Boolean
+
+    Private Sub Form_Load() Handles MyBase.Load
+        Me.KeyPreview = True
+        evaluated = True
+    End Sub
+
+    Private Sub Form_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyClass.KeyPress
+
+        Select Case e.KeyChar
+            Case "+"
+                op1 = TextBox1.Text
+                operate = "+"
+                prevCalc.Text = TextBox1.Text + " +"
+                TextBox1.Text = ""
+                evaluated = False
+            Case "-"
+                op1 = TextBox1.Text
+                operate = "-"
+                prevCalc.Text = TextBox1.Text + " -"
+                TextBox1.Text = ""
+                evaluated = False
+            Case "*"
+                op1 = TextBox1.Text
+                operate = "*"
+                prevCalc.Text = TextBox1.Text + " *"
+                TextBox1.Text = ""
+                evaluated = False
+            Case "/"
+                op1 = TextBox1.Text
+                operate = "/"
+                prevCalc.Text = TextBox1.Text + " /"
+                TextBox1.Text = ""
+                evaluated = False
+        End Select
+
+        If evaluated = False Then
+            Select Case e.KeyChar
+                Case "1"
+                    TextBox1.Text = TextBox1.Text + "1"
+                Case "2"
+                    TextBox1.Text = TextBox1.Text + "2"
+                Case "3"
+                    TextBox1.Text = TextBox1.Text + "3"
+                Case "4"
+                    TextBox1.Text = TextBox1.Text + "4"
+                Case "5"
+                    TextBox1.Text = TextBox1.Text + "5"
+                Case "6"
+                    TextBox1.Text = TextBox1.Text + "6"
+                Case "7"
+                    TextBox1.Text = TextBox1.Text + "7"
+                Case "8"
+                    TextBox1.Text = TextBox1.Text + "8"
+                Case "9"
+                    TextBox1.Text = TextBox1.Text + "9"
+                Case "0"
+                    TextBox1.Text = TextBox1.Text + "0"
+                Case "."
+                    TextBox1.Text = TextBox1.Text + "."
+            End Select
+        End If
+
+        If evaluated = True Then
+            Select Case e.KeyChar
+                Case "1"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "1"
+                    evaluated = False
+                Case "2"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "2"
+                    evaluated = False
+                Case "3"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "3"
+                    evaluated = False
+                Case "4"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "4"
+                    evaluated = False
+                Case "5"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "5"
+                    evaluated = False
+                Case "6"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "6"
+                    evaluated = False
+                Case "7"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "7"
+                    evaluated = False
+                Case "8"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "8"
+                    evaluated = False
+                Case "9"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "9"
+                    evaluated = False
+                Case "0"
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "0"
+                    evaluated = False
+                Case "."
+                    prevCalc.Text = TextBox1.Text
+                    TextBox1.Text = "0."
+                    evaluated = False
+            End Select
+        End If
+
+
+
+    End Sub
+
+
+    Private Sub eql_Click(sender As Object, e As EventArgs) Handles eqBtn.Click
+        Dim solution As Integer
+        op2 = TextBox1.Text
+
+        Select Case operate
+            Case "+"
+                prevCalc.Text = op1.ToString + " + " + op2.ToString
+                TextBox1.Text = op1 + op2
+                evaluated = True
+            Case "-"
+                prevCalc.Text = op1.ToString + " - " + op2.ToString
+                TextBox1.Text = op1 - op2
+                evaluated = True
+            Case "*"
+                prevCalc.Text = op1.ToString + " * " + op2.ToString
+                TextBox1.Text = op1 * op2
+                evaluated = True
+            Case "/"
+                prevCalc.Text = op1.ToString + " / " + op2.ToString
+                TextBox1.Text = op1 / op2
+                evaluated = True
+
+        End Select
+    End Sub
+
+    Private Sub b1_Click(sender As Object, e As EventArgs) Handles b1.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "1"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "1"
+            evaluated = False
+        End If
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles signBtn.Click
+        Dim currentNum As Double
+        If TextBox1.Text IsNot "" Then
+            currentNum = CDbl(TextBox1.Text)
+        End If
+
+
+        If currentNum < 0 Then
+            currentNum = currentNum + (2 * Math.Abs(currentNum))
+        ElseIf currentNum > 0 Then
+            currentNum = currentNum - (2 * currentNum)
+        End If
+
+        TextBox1.Text = currentNum
+
+    End Sub
+
+    Private Sub clrBtn_Click(sender As Object, e As EventArgs) Handles clrBtn.Click
+        op1 = 0
+        op2 = 0
+        operate = ""
+        TextBox1.Text = 0
+        prevCalc.Text = ""
+        evaluated = True
+    End Sub
+
+    Private Sub bkBtn_Click(sender As Object, e As EventArgs) Handles bkBtn.Click
+        Try
+            TextBox1.Text = TextBox1.Text.Substring(0, TextBox1.Text.Length - 1)
+        Catch exception As System.ArgumentOutOfRangeException
+            MsgBox("Nothing to delete...")
+        End Try
+
+    End Sub
+
+    Private Sub b2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "2"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "2"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b3_Click(sender As Object, e As EventArgs) Handles b3.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "3"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "3"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b4_Click(sender As Object, e As EventArgs) Handles b4.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "4"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "4"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b5_Click(sender As Object, e As EventArgs) Handles b5.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "5"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "5"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b6_Click(sender As Object, e As EventArgs) Handles b6.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "6"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "6"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b7_Click(sender As Object, e As EventArgs) Handles b7.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "7"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "7"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b8_Click(sender As Object, e As EventArgs) Handles b8.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "8"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "8"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b9_Click(sender As Object, e As EventArgs) Handles b9.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "9"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "9"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub b0_Click(sender As Object, e As EventArgs) Handles b0.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "0"
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "0"
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub bDot_Click(sender As Object, e As EventArgs) Handles bDot.Click
+        If evaluated = False Then
+            TextBox1.Text = TextBox1.Text + "."
+        ElseIf evaluated = True Then
+            prevCalc.Text = TextBox1.Text
+            TextBox1.Text = "0."
+            evaluated = False
+        End If
+    End Sub
+
+    Private Sub bPlus_Click(sender As Object, e As EventArgs) Handles bPlus.Click
+        op1 = TextBox1.Text
+        operate = "+"
+        prevCalc.Text = TextBox1.Text + " +"
+        TextBox1.Text = ""
+        evaluated = False
+    End Sub
+
+    Private Sub bMinus_Click(sender As Object, e As EventArgs) Handles bMinus.Click
+        op1 = TextBox1.Text
+        operate = "-"
+        prevCalc.Text = TextBox1.Text + " -"
+        TextBox1.Text = ""
+        evaluated = False
+    End Sub
+
+    Private Sub bMult_Click(sender As Object, e As EventArgs) Handles bMult.Click
+        op1 = TextBox1.Text
+        operate = "*"
+        prevCalc.Text = TextBox1.Text + " *"
+        TextBox1.Text = ""
+        evaluated = False
+    End Sub
+
+    Private Sub bDiv_Click(sender As Object, e As EventArgs) Handles bDiv.Click
+        op1 = TextBox1.Text
+        operate = "/"
+        prevCalc.Text = TextBox1.Text + " /"
+        TextBox1.Text = ""
+        evaluated = False
+    End Sub
+
+    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        Application.Exit()
+
+    End Sub
+End Class
